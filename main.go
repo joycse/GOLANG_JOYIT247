faf7d4554d33ea4d0fb2b85fb497450f075a4b32
“joycse” <joycseiu@gmail.com>
2021-02-06 22:50:15 +0600

    one print line change

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
-
 import "fmt"
 
-func main(){
+func main() {
 
-fmt.Println("Hello Bangladesh")
+	fmt.Println("Hello Bangladesh")
+	fmt.Println("Just make one change to push from local to remote github account")
 }
