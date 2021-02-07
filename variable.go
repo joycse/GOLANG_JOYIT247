1cfb7c320c509eee66e092cc0b821cd0dc3e5ec1
“joycse” <joycseiu@gmail.com>
2021-02-07 15:25:49 +0600

    variable.go initial stage

---
+++ variable.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var name string = "JOY"
+	var number int = 500
+	var char rune = 'A'
+	var money float32 = 500.525
+
+	fmt.Println(name, number, char, money)
+}
