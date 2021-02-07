ea50499d3679fcb1a6efb44c02a56390eb906d09
“joycse” <joycseiu@gmail.com>
2021-02-07 20:20:24 +0600

    Initintial variable

---
+++ bool.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	var isFound bool
+	fmt.Println(isFound)
+}
---
+++ super_hero.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var superman string
+	superman = "I am a superman"
+	fmt.Println(superman)
+
+	var batman string = "i am a batman"
+	fmt.Println(batman)
+
+	var thor :="I am a thor"
+	fmt.Println(thor)
+}
